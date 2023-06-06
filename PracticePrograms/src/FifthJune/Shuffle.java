7b742961deaff1f0900f02cecd7dffd687d6f1ff
Amruta Dhage <amruta.dhage@excellarate.com>
2023-06-06 18:25:22 +0530

    Resolved the comments

--- PracticePrograms/src/FifthJune/Shuffle.java
+++ PracticePrograms/src/FifthJune/Shuffle.java
@@ -5,23 +5,23 @@ import java.util.Random;
 
 public class Shuffle {
 
-    public static void shuffle(int numbers[]) {
+    public static void shuffleNumbers(int numbers[]) {
         for (int index = numbers.length - 1; index >= 1; index--) {
             Random rand = new Random();
             int result = rand.nextInt(index + 1);
             swap_elements(numbers, index, result);
         }
     }
-    private static void swap_elements(int[] numbers, int index, int result) {
-        int temp = numbers[index];
-        numbers[index] = numbers[result];
-        numbers[result] = temp;
+    private static void swap_elements(int[] arrayNumbers, int index, int result) {
+        int temp = arrayNumbers[index];
+        arrayNumbers[index] = arrayNumbers[result];
+        arrayNumbers[result] = temp;
     }
     public static void main(String[] args) {
         {
             int[] numbers = {1, 2, 3, 4, 5, 6};
             System.out.println("Original Array: " + Arrays.toString(numbers));
-            shuffle(numbers);
+            shuffleNumbers(numbers);
             System.out.println("Shuffle Array: " + Arrays.toString(numbers));
         }
     }
