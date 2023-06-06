938893082bea24b2488d38986e3e8f85a3e46ea5
Amruta Dhage <amruta.dhage@excellarate.com>
2023-06-06 20:32:09 +0530

    Matrix_Addition

--- PracticePrograms/src/SixthJune/MatrixAddition.java
+++ PracticePrograms/src/SixthJune/MatrixAddition.java
@@ -3,6 +3,7 @@ package SixthJune;
 public class MatrixAddition {
 
     public static void main(String args[]) {
+
         int firstMatrix[][] = {{1, 3, 4}, {2, 4, 3}, {3, 4, 5}};
         int secondMatrix[][] = {{1, 3, 4}, {2, 4, 3}, {1, 2, 4}};
 
@@ -10,10 +11,10 @@ public class MatrixAddition {
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                sumOfMatrices[i][j] = firstMatrix[i][j] + secondMatrix[i][j];  //use - for subtraction
+                sumOfMatrices[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
                 System.out.print(sumOfMatrices[i][j] + " ");
             }
-            System.out.println();//new line
+            System.out.println();
         }
     }
 }
