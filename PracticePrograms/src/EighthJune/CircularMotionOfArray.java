103caa3e8a787a6cb540c8d78ab4b439f4247b23
Amruta Dhage <amruta.dhage@excellarate.com>
2023-06-19 15:17:00 +0530

    Updated as per Suyog's Comment

--- PracticePrograms/src/EighthJune/CircularMotionOfArray.java
+++ PracticePrograms/src/EighthJune/CircularMotionOfArray.java
@@ -1,24 +1,27 @@
 package EighthJune;
 
+import java.util.Arrays;
+
 public class CircularMotionOfArray {
 
-    static void leftRotate(int arrayToBeRotated[], int numberOfPositionsToRotate, int SizeOfTheArray) {
+    public static void leftRotate(int arrayToBeRotated[], int numberOfPositionsToRotate, int SizeOfTheArray) {
         for (int index = 0; index < numberOfPositionsToRotate; index++)
             leftRotatebyOne(arrayToBeRotated, SizeOfTheArray);
     }
-    static void leftRotatebyOne(int arrayToBeRotated[], int SizeOfTheArray) {
+    public static void leftRotatebyOne(int arrayToBeRotated[], int SizeOfTheArray) {
         int index, temporary;
         temporary = arrayToBeRotated[0];
         for (index = 0; index < SizeOfTheArray - 1; index++) arrayToBeRotated[index] = arrayToBeRotated[index + 1];
         arrayToBeRotated[SizeOfTheArray - 1] = temporary;
     }
-    static void printArray(int arrayToBeRotated[], int SizeOfTheArray) {
+    public static void printArray(int arrayToBeRotated[], int SizeOfTheArray) {
         for (int index = 0; index < SizeOfTheArray; index++) System.out.print(arrayToBeRotated[index] + " ");
     }
     public static void main(String[] args) {
-        // RotateArray rotate = new RotateArray();
         int arrayToBeRotated[] = {1, 2, 3, 4, 5};
+        System.out.println("Original array: " + Arrays.toString(arrayToBeRotated));
         leftRotate(arrayToBeRotated, 2, 5);
+        System.out.println("Rotated array: ");
         printArray(arrayToBeRotated, 5);
     }
 }
